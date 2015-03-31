6918c93fb0f078e13c32e4e15a14543b7047980d
YIN Yan <i@yinyan.fr>
2015-03-31 20:04:41 +0200

    sponsor page final

--- UI/ui.js
+++ UI/ui.js
@@ -8,7 +8,7 @@ var SponsorList = [];
 
 SponsorList[0] = "Novel Script";
 SponsorList[1] = "这是一个基于javascript的";
-//SponsorList[2] = "Galgame引擎";
+SponsorList[2] = "lib/lx1.png";
 //SponsorList[3] = "刚刚开始制作";
 
 
@@ -18,20 +18,23 @@ function portrait() {
 }
 
 function middleLine(target) {
+    var heightToTop = 0.45;
     var targetWidth = (function () {
         if (target.constructor == String) {
             if (target.slice(-3) == "jpg" || target.slice(-3) == "png") {
-                $("#sponsor").html("<img src=" + target + " />");
+                $("#sponsor").html("<img id=sponimg src=" + target + " />");
+                heightToTop = 0.5 * (($(window).height() - $("#sponsor").height())/$(window).height());
                 return $("#sponsor").width() + "px";
             }
             else {
                 $("#sponsor").html(target);
+                heightToTop = 0.5 * (($(window).height() - $("#sponsor").height())/$(window).height());
                 return target.length + "em";
             }
         }
     })();
     $("#sponsor").css("margin-top", function () {
-            return 0.45 * $(window).height() + "px";
+            return heightToTop * $(window).height() + "px";
         });
 
 }
