06b8ceb62bd3807ca12e21ec5cdd348b160543b0
YIN Yan <i@yinyan.fr>
2015-03-31 19:33:16 +0200

    page sponsor done

--- UI/ui.js
+++ UI/ui.js
@@ -6,20 +6,22 @@ var UniverseWindowWidth = 800;
 
 var SponsorList = [];
 
-SponsorList[0] = "这是一个名字";
-SponsorList[1] = "这是另一名字";
+SponsorList[0] = "Novel Script";
+SponsorList[1] = "这是一个基于javascript的";
+SponsorList[2] = "Galgame引擎";
+SponsorList[3] = "刚刚开始制作";
 
 
 // Methods
-function portrait(){
+function portrait() {
     return (($(window).width() < UniverseWindowWidth) || ($(window).width < $(window).height()))
 }
 
-function middleLine(target){
-    var targetWidth = (function(){
-        if(target.constructor == String){
-            if(target.slice(-3) == "jpg" || target.slice(-3) == "png"){
-                $("#sponsor").html("<img src="+target+" />");
+function middleLine(target) {
+    var targetWidth = (function () {
+        if (target.constructor == String) {
+            if (target.slice(-3) == "jpg" || target.slice(-3) == "png") {
+                $("#sponsor").html("<img src=" + target + " />");
                 return $("#sponsor").width() + "px";
             }
             else {
@@ -28,26 +30,36 @@ function middleLine(target){
             }
         }
     })();
-    $("#sponsor").css("margin-left","auto")
-        .css("margin-right","auto")
-        .css("width",targetWidth)
-        .css("margin-top",function(){
-       return 0.45 * $(window).height() + "px";
+    $("#sponsor").css("margin-top", function () {
+            return 0.45 * $(window).height() + "px";
+        });
+
+}
+
+function windowResize() {
+    $("#sponsor").css("margin-top", function () {
+        return 0.45 * $(window).height() + "px";
     });
 }
 
-function welcome(l){
-    if(l.length == 0) return;
-    else{
+function welcome(l) {
+    if (l.length == 0) return;
+    else {
         var tmp = l.shift();
         middleLine(tmp);
-        $("#sponsor").fadeIn(1500,function(){
-            $("#sponsor").fadeOut(1500,welcome(l));
+        $("#sponsor").fadeIn(1500, function () {
+            $("#sponsor").fadeOut(1500,function(){
+                welcome(l);
+            });
+
         });
+
     }
 
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
+    $(window).resize(windowResize());
+
     welcome(SponsorList);
 });
