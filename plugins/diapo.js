ad81e76438e211db61a1f5330da3c5bfebe08f76
yan <i@yinyan.fr>
2015-11-27 14:53:40 +0100

    这个项目似乎重新开始了

--- plugins/diapo.js
+++ plugins/diapo.js
@@ -1,72 +1,100 @@
 /**
- * Created by Ian on 2015/5/15.
- * require jQuery 1.11.2, align.js
- * effect plugin diapo for NovelScript 1.0
+ * Created by Ian on 2015/11/27.
+ * requiring jQuery 1.11.3, align.js, preload.js
+ * effect plugin diapo for NovelScript 1.0 "Hina"
  * creating a serveral slice of diapo in a vacant element aginst its parent element
  */
 
-var diapo = function(list,stage,bgColor,time){
+/**
+ * Create a diaporama
+ * @param list : Array of texts and pictures
+ * @param $stage : $, container of the diapo
+ * @param bgColor : string, background color to change if needed
+ * @param time : int, time of each slide
+ * @param iter
+ * @param stop
+ */
+var diapo = function (list, $stage, bgColor, time, iter, stop) {
     var diapo = {};
-    diapo.selector = stage;
-    diapo.list = list;
-    diapo.stage = "#diapostage";
-    diapo.time = time||1500;
-    diapo.parent = $(stage);
-    diapo.bgColorCache = diapo.parent.css("background-color");
-    diapo.init = function(){
-        $(stage).html("<div id='diapostage'></div>");
-        //alert($(stage).html());
-        diapo.parent.css("background-color",bgColor);
-        Align.full(diapo.stage);
-        $(diapo.stage).hide();
-        return this;
+    diapo.originBgColor = $stage.css("background-color");
+    diapo.isImg = function (element) {
+        return element.slice(-3) == "jpg" || element.slice(-3) == "png"
     };
-    diapo.recover = function(afterwards){
-        diapo.parent.css("background-color",diapo.bgColorCache);
-        afterwards();
+    /**
+     * preload pictures from the list before launch
+     * @param callback
+     */
+    diapo.preload = function (callback) {
+        preloadImgMain(list.filter(diapo.isImg), callback)
     };
-    diapo.toHtml = function(n) {
-        if (n.slice(-3) == "jpg" || n.slice(-3) == "png") {
-            return "<img src=" + n + " />"
-        }
+    diapo.$slide = $("<div></div>").hide();
+    diapo.slideRec = function (list, callback) {
+        console.log("rec");
+        if (!list.length) diapo.recover(callback);
         else {
-            return n;
+            var thisSlide = list[0];
+            diapo.$slide.html((function () {
+                if (diapo.isImg(thisSlide)) {
+                    return $("<img />").attr("src", thisSlide)
+                } else return thisSlide
+            })());
+            var tmpWidth = diapo.$slide.width();
+            Align.full(diapo.$slide);
+            diapo.$slide.width(tmpWidth);
+            diapo.$slide.fadeIn(time, function () {
+                diapo.$slide.fadeOut(time, function () {
+                    diapo.slideRec(list.slice(1), callback)
+                })
+            })
         }
     };
-    diapo.anime = function(afterwards){
-        time = diapo.time;
-        if(diapo.list.length === 0){
-            diapo.recover(afterwards);
-        }
-        else{
-            var tmp = diapo.list.shift();
-            $(diapo.stage).html(diapo.toHtml(tmp));
-            //alert($(diapo.stage).html())
-            var tmpWidth = $(diapo.stage).width();
-            Align.full(diapo.stage);
-            $(diapo.stage).width(tmpWidth);
-            $(diapo.stage).fadeIn(time, function () {
-                //TODO bug of width fixed but should it be in align.js?
+    diapo.slideIter = function (list, callback) {
+        var slideIterate = function (i) {
+            var thisSlide = list[i];
+            diapo.$slide.html((function () {
+                if (diapo.isImg(thisSlide)) {
+                    return $("<img />").attr("src", thisSlide)
+                } else return thisSlide
+            })());
+            var tmpWidth = diapo.$slide.width();
+            Align.full(diapo.$slide);
+            diapo.$slide.width(tmpWidth);
+            diapo.$slide.fadeIn(time, function () {
+                diapo.$slide.fadeOut()
+            })
+        };
 
-                $(diapo.stage).fadeOut(time,function(){
-                    diapo.anime(afterwards);
-                });
-            });
-        }
-        return this;
+        var counter = function () {
+            var i = 0;
+            return function () {
+                slideIterate(i);
+                i++;
+                if (i >= list.length) i = 0;
+            }
+        };
+        counter();
+        var _iterator = setInterval(counter(), 2 * time)
+
     };
-    diapo.execute = function(afterwards){
-        diapo.init().anime(afterwards);
+    diapo.recover = function(callback){
+        $stage.css("background-color", diapo.originBgColor);
+        callback();
     };
-    return diapo;
+    diapo.execute = function (callback) {
+        console.log("hi");
+        $("#stage").append(diapo.$slide);
+        Align.full(diapo.$slide);
+        $stage.css("background-color", bgColor);
+        if(!iter) diapo.slideRec(list, callback);
+        else diapo.slideIter(list, callback);
+    };
+    return diapo
 };
 
-/*
- var l = ["hgah","dfsgsfdghsf","dgfsdf"];
 
- var welcome = diapo(l,"#b","gray");
+ var l = ["hgah","dfsgsfdghsf","dgfsdf"];
+ var welcome = diapo(l, $("#stage"), "gray", 1000, true);
 
  $(document).ready(function () {
- welcome.execute(1500,function(){return 0});
+ welcome.execute(function(){return 0});
  });
- */
