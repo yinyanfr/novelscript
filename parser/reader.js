261e1e23148f356a9694d39d1ca7db4a4681603e
YIN <i@yinyan.fr>
2016-02-22 19:17:18 +0100

    为了看起来愉悦一点，parser另开一个repo
    https://github.com/yinyanfr/NovelScript-Parser

--- parser/reader.js
+++ parser/reader.js
@@ -3,12 +3,8 @@
  */
 
 // this is a node.js file
-var fs = require("fs");
-fs.readFile("./test.txt", "utf-8", function (err, data) {
-    if(err) throw err;
-    data = data.toString().split("\n");
-    while(data[data.length-1] == ""){
-        data.length--
-    }
-    console.log(data)
-});
+
+/*
+  in progress
+  https://github.com/yinyanfr/NovelScript-Parser
+ */
