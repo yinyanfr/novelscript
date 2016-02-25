f2096d64f491a4c53147a760debdeb3c8d3f55d3
YIN <i@yinyan.fr>
2016-02-25 19:20:39 +0100

    shrink version a

--- plugins/shrink.js
+++ plugins/shrink.js
@@ -5,56 +5,42 @@
  */
 
 /**
- * plugin NovelScript, target will shrink with mouse or automatically
+ * plugin NovelScript, target will shrink automatically
  * TODO bugs remaining
  * @param $target : $, target that will shrink
- * @param limit : float, rate that target react against the movement of mouse
- * @param auto : boolean, true if target will shrink automatically
- * @param time : int, frequency of movement when automatically
+ * @param distance : float, rate that target react against the movement of mouse
+ * @param count : int, frequency of movement when automatically
  */
-function shrink($target, limit, auto, time){
+function shrink($target, distance, count) {
     var shrink = {};
-    var randint = function(start, end){
-        return start + (end - start) * Math.random()
+    distance = 4;
+    count = count || 4;
+    var step = 0;//计数器
+    var off = $target.offset();
+
+    function pos() {
+        shrink.step = shrink.step || 0;
+        shrink.step = shrink.step == 4 ? 0 : shrink.step;
+        var set = {
+            0: {x: 0, y: -1},
+            1: {x: -1, y: 0},
+            2: {x: 0, y: 1},
+            3: {x: 1, y: 0}
+        };
+        return set[shrink.step++];
+    }
+
+    shrink.execute = function () {
+        shrink.step = 0;
+        var timer = setInterval(function () {
+            var set = pos();
+            $target.offset({top: off.top + set.y * len, left: off.left + set.x * len});
+            if (step++ >= c) {
+                $target.offset({top: off.top, left: off.left});
+                clearInterval(timer);
+            }
+        }, 45);
     };
-    /**
-     * function io rec, movement when moving automatically
-     * @param x
-     * @param y
-     * @param time
-     */
-    shrink.move = function (x, y, time) {
-        $target.animate({
-            "left": "+="+x,
-            "top": "+="+y
-        }, time)
-    };
-    shrink.execute = function(callback){
-        $target.width($(window).width() * (1 + limit))
-            .height($(window).height() * (1 + limit));
-        if(auto){
-            // use interval to move
-            setInterval(function () {
-                var x = randint(0, $(window).width());
-                var y = randint(0, $(window).height());
-                shrink.move(x, y, time)
-            }, time)
-        }else{
-            // move listening to mouse
-            $target.mousemove(function (event) {
-                event.stopPropagation();
-                $target.css({
-                    "left": "-=" + ($target.width() - event.pageX) - $(window).width(),
-                    "top": "-=" + ($target.height() - event.pageY) - $(window).height()
-                })
-            })
-        }
-        if(callback){
-            $target.one("click", function (event) {
-                event.stopPropagation();
-                callback();
-            })
-        }
-    };
+
     return shrink
 }
